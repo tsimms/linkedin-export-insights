c55007f0c85ec15d49e157e585ccceb3bd3f29a4
tsimms <tim.simms@influentmetrics.com>
2024-01-26 05:06:39 -0500

    turn off vite dev

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: {
     headers: {
-      //'Cross-Origin-Embedder-Policy': 'require-corp',
+      'Cross-Origin-Embedder-Policy': 'credentialless',
       'Cross-Origin-Opener-Policy': 'same-origin',
     },
   },

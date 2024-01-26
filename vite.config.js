aaef4da828d8a10e7919369ed9beae0d01cb1992
tsimms <tim.simms@influentmetrics.com>
2024-01-26 14:25:25 -0500

    trying with a subfolder

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/linkedinsight/',
   server: {
     headers: {
       'Cross-Origin-Embedder-Policy': 'credentialless',

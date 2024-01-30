9a6beeaabce919e8c5abf194e31502a12a3b8cdc
tsimms <tim.simms@influentmetrics.com>
2024-01-30 14:45:25 -0500

    broaden scope

--- app.js
+++ app.js
@@ -286,7 +286,8 @@ const bootstrapServer = async () => {
   console.log(`Server URL: ${_serverUrl}!`);
 
   if ('serviceWorker' in navigator) {  
-    await navigator.serviceWorker.register(`./gql-intercept.js?serverUrl=${_serverUrl}`)
+    await navigator.serviceWorker
+      .register(`./gql-intercept.js?serverUrl=${_serverUrl}`, { scope: '/' })
       .then(registration => {
         console.log('Service Worker registered with scope:', registration.scope);
   

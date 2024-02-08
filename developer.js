840dc87aacc936671c4feb0e3b968f58a261a03f
tsimms <tim.simms@influentmetrics.com>
2024-02-08 11:21:08 -0500

    refactor to split dev frame code

---
+++ developer.js
@@ -0,0 +1,203 @@
+import gqlContainer from './gql-container.js';
+import { ApolloSandbox } from '@apollo/sandbox';
+import './developer.css';
+
+let webcontainerInstance, _serverUrl;
+
+
+///////////
+// Developer Frame UX
+///////////
+
+const showLoading = () => {
+  const sandbox = document.getElementById('spinner');
+  sandbox.classList.remove('hide');
+};
+
+const hideLoading = () => {
+  const sandbox = document.getElementById('spinner');
+  sandbox.classList.add('hide');
+}
+
+const showExplore = () => {
+  const explore = document.getElementById('explore-dashboard');
+  explore.classList.remove('hide');
+  document.getElementById('bridge').innerHTML = `
+  <iframe id="bridge-frame" style="width:1px; height:1px; border:none;" src="${_serverUrl}/bridge"></iframe>
+  `;
+}
+
+const developerFrameUx = () => {
+  const panel = document.getElementById("developer-frame");
+  let m_pos;
+
+  const resizeDeveloperFrame = (e) => {
+    const dy = m_pos - e.y;
+    m_pos = e.y;
+    panel.style.height = (parseInt(getComputedStyle(panel, '').height) + dy) + "px";
+
+    // TODO: resize filler div
+  };
+  
+  panel.addEventListener("mousedown", function(e){
+    if (e.offsetY < 4) {
+      m_pos = e.y;
+      document.addEventListener("mousemove", resizeDeveloperFrame, false);
+    }
+  }, false);
+  
+  document.addEventListener("mouseup", function(){
+      document.removeEventListener("mousemove", resizeDeveloperFrame, false);
+  }, false);
+
+  document.getElementById('run-query').addEventListener('click', runQuery)
+
+  document.getElementById('btn-copy').addEventListener('click', async () => {
+    const textarea = document.getElementById('explore-results');
+    textarea.focus();
+    await navigator.clipboard.writeText(textarea.value);
+    const button = document.getElementById('btn-copy')
+    button.innerHTML = 'Copied!';
+    button.disabled = true;
+    setTimeout(() => { button.innerHTML = 'Copy'; button.disabled = false; }, 2000);
+  });
+  
+  document.getElementById('explore-query').addEventListener('input', (e) => {
+    const textarea = e.target;
+    setTimeout(() => { textarea.value = textarea.value.replace(/\\n/g, '\n') }, 2000);
+  });
+}
+
+////////////
+// Server
+////////////
+const launchServer = async (uploadedFile) => {
+  showLoading();
+  const server = await gqlContainer({ 
+    dataFileInput:uploadedFile,
+    staticFiles: [
+      `https://timjimsimms.com/linkedinsight/server/server.js`,
+      `https://timjimsimms.com/linkedinsight/server/graphql-model.js`,
+      `https://timjimsimms.com/linkedinsight/server/data.js`,
+      `https://timjimsimms.com/linkedinsight/server/package.json`
+    ],
+    debug:false
+  });
+  ({ webcontainerInstance } = server);
+  _serverUrl = server.serverUrl;
+
+  console.log(`Server URL: ${_serverUrl}!`);
+  hideLoading();
+  showExplore();
+
+/*
+  // the reason this doesn't work is because the /sandbox/explorer endpoint doesn't include a
+  // CORP header ("Cross-Origin-Resource-Policy: cross-origin"), which is needed in this
+  // context, because we have a COEP policy on the document for the webcontainer execution
+  new ApolloSandbox({
+    target: '#embedded-sandbox',
+    initialEndpoint: `${_serverUrl}/graphql?serverUrl=${_serverUrl}`,
+    initialState: {
+    }
+  });
+  */
+
+/*
+ document.getElementById('embedded-sandbox').innerHTML = `
+  <iframe style="width:100%; height:100%"
+    src="${_serverUrl}?serverUrl=${_serverUrl}"
+    ></iframe>
+  `;
+  */
+/*
+  document.getElementById('embedded-sandbox').innerHTML = `
+  <iframe style="width:100%; height:100%"
+    src="${_serverUrl}/inigo"
+    ></iframe>
+  `;
+*/
+  /*
+  {
+    "welcomeModal": [],
+    "explorer": {
+        "url": serverUrl,
+        "activeTabId": "01d3",
+        "tabs": [
+            {
+                "id": "01d3",
+                "query": "",
+                "variables": "{}",
+                "docLastQuery": ""
+            }
+        ],
+        "collections": [],
+        "preflightScript": "",
+        "history": [],
+        "headers": "",
+        "preflightEnabled": true,
+        "envVariables": "",
+        "proxyEnabled": false,
+        "historyEnabled": true
+    },
+    "theme": "dark"
+}
+
+*/
+
+//  window.open(_serverUrl, '_blank');
+}
+
+// Query response message handler
+window.addEventListener('message', (event) => {
+  const { data, origin } = event;
+  if (origin === _serverUrl) {
+    try {
+      const { type, results, message } = JSON.parse(data);
+      if (type === 'bridge_response') {
+        const resultsElement = document.getElementById('explore-results');
+        resultsElement.value = JSON.stringify(results, undefined, 2);
+        if (results.data[Object.keys(results.data)[0]].length) {
+          document.getElementById('results-status').classList.remove('hide');
+          document.getElementById('results-count').innerHTML = `Results count: ${results.data[Object.keys(results.data)[0]].length}`;
+        } else {
+          document.getElementById('results-status').classList.add('hide');
+        }
+        return
+      } else if (type === 'bridge_error') {
+        console.error(message);
+      }
+    } catch (e) {}
+    console.error('something went wrong on response');
+  }
+});
+
+
+
+const runQuery = async () => {
+  const query = document.getElementById('explore-query')
+    .value
+    .replaceAll('\n',"")
+    .replaceAll(/[ ]+/g," ");
+
+  document.getElementById('bridge-frame').contentWindow.postMessage(JSON.stringify({
+    url: _serverUrl, 
+    method: 'POST',
+    headers: {
+      "Content-type": "application/json"
+    },
+    body: query
+  }), _serverUrl);
+}
+
+
+////////
+// Main
+////////
+
+
+// Init
+(() => {
+    developerFrameUx();
+})()
+
+export { launchServer };

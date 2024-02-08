ebcbb4ac1c7f723e6e58ba347669f0611acc300c
tsimms <tim.simms@influentmetrics.com>
2024-02-07 19:33:06 -0500

    separate functions

--- server/server.js
+++ server/server.js
@@ -146,7 +146,8 @@ const startApolloServer = async () => {
         const res = await fetch(url, { method, headers, body });
         if (res.ok) {
           debugger;
-          const results = JSON.parse(await res.json());
+          const results_json = await res.json();
+          const results = JSON.parse(results_json);
           console.log('BRIDGE: sending response from successful query');
           window.parent.postMessage(JSON.stringify({ type: 'bridge_response', results }), origin);
         } else {

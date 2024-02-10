cee848a6794f8ce1eef78a7e6e9d575b0fbc1fce
tsimms <tim.simms@influentmetrics.com>
2024-02-09 22:13:36 -0500

    debug

--- developer.js
+++ developer.js
@@ -321,6 +321,7 @@ const processIntrospectionData = (data) => {
     const fields = _schema.types.find(t => t.name === objectType).fields
     let fieldText = '';
     if (fields) {
+      debugger;
       fieldText = fields.map(f => (`\t${f.name}${f.type.ofType === 'LIST' 
         ? ` {\n${_schema.types.find(tt => tt.name === f.type.ofType.ofType.ofType.name)?.fields
           .map(ff => (`${ff.type.ofType !== 'LIST' ? `\t\t${ff.name}\n` : ""}`))

93336ad1baf4162b099fb0ed5afb1e96d9f7f1e4
tsimms <tim.simms@influentmetrics.com>
2024-02-11 15:59:32 -0500

    escape slashes

--- server/server.js
+++ server/server.js
@@ -136,7 +136,7 @@ const startApolloServer = async () => {
         globalAPI: false,
         getWorkerUrl : function (moduleId, label) {
           var result =  paths[label];
-          if (/^((http:)|(https:)|(file:)|(\/\/))/.test(result)) {
+          if (/^((http:)|(https:)|(file:)|(\\\/\\\/))/.test(result)) {
             var currentUrl = String(window.location);
             var currentOrigin = currentUrl.substr(0, currentUrl.length - window.location.hash.length - window.location.search.length - window.location.pathname.length);
             if (result.substring(0, currentOrigin.length) !== currentOrigin) {

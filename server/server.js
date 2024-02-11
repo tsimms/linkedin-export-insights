e708f315471671309a608c37608ce6591e35497e
tsimms <tim.simms@influentmetrics.com>
2024-02-11 15:44:36 -0500

    include monaco code

--- server/server.js
+++ server/server.js
@@ -130,6 +130,32 @@ const startApolloServer = async () => {
 <html>
   <head>
     <base href="https://explorer.inigo.io">
+    <script>
+    self["MonacoEnvironment"] = (function (paths) {
+      return {
+        globalAPI: false,
+        getWorkerUrl : function (moduleId, label) {
+          var result =  paths[label];
+          if (/^((http:)|(https:)|(file:)|(\/\/))/.test(result)) {
+            var currentUrl = String(window.location);
+            var currentOrigin = currentUrl.substr(0, currentUrl.length - window.location.hash.length - window.location.search.length - window.location.pathname.length);
+            if (result.substring(0, currentOrigin.length) !== currentOrigin) {
+              var js = '/*' + label + '*/importScripts("' + result + '");';
+              var blob = new Blob([js], { type: 'application/javascript' });
+              return URL.createObjectURL(blob);
+            }
+          }
+          return result;
+        }
+      };
+    })({
+      "json": "/monacoeditorwork/json.worker.bundle.js",
+      "typescript": "/monacoeditorwork/ts.worker.bundle.js",
+      "editorWorkerService": "/monacoeditorwork/editor.worker.bundle.js",
+      "graphql": "/monacoeditorwork/graphql.worker.bundle.js",
+      "javascript": "/monacoeditorwork/ts.worker.bundle.js"
+    });
+    </script>
     <script type="module" crossorigin src="/assets/index-6211fc1a.js"></script>
     <link rel="stylesheet" crossorigin href="/assets/index-c80688b3.css">
   </head>
